02e4404f001bfd426e18ab4f107aad62724f498c
NecroBread <mariovyordanov@gmail.com>
2022-10-16 22:05:44 +0300

    update mongoose errors response

--- src/utils/mapErrors.js
+++ src/utils/mapErrors.js
@@ -1,6 +1,10 @@
+const mongoose = require('mongoose');
+
 module.exports = (err) => {
     if (Array.isArray(err)) {
         return err;
+    } else if (err instanceof mongoose.Error) {
+        return Object.values(err.errors).map(e => e.message);
     } else if (typeof err === 'string') {
         return [err];
     } else if (err.message) {
