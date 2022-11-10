f4be5791af902c1bde654d8fbf966ede6802eded
NecroBread <mariovyordanov@gmail.com>
2022-11-10 17:12:10 +0200

    kinda finish the awkward global cat chat

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,17 +1,39 @@
+const jwt = require('jsonwebtoken');
+
 module.exports = (io) => {
     const messageIo = io.of('/socket/message')
 
     messageIo.use((socket, next) => {
-        next();
+        const cookie = socket.request.headers.cookie
+        authenticateToken(cookie, socket, next);
     })
 
     messageIo.on('connection', socket => {
-
-        socket.on('message', (msg) => {
-            messageIo.emit('message', `${msg}`);
+        socket.on('message', (msgObj) => {
+            messageIo.emit('message', msgObj);
         })
 
-        socket.on('disconnect', () => console.log('disconnected'));
+        socket.broadcast.emit('message', 'A user has joined')
 
+        socket.on('disconnect', () => {
+            messageIo.emit('message', 'A user has left the chat')
+        });
+
+    })
+}
+
+function authenticateToken(cookie, socket, next) {
+    const [, accessToken] = cookie.split('=');
+
+    if (!accessToken) {
+        socket.disconnect(1);
+    }
+
+    jwt.verify(accessToken, process.env.JWT_SECRET, (err) => {
+        if (err) {
+            socket.disconnect(1);
+        } else {
+            next();
+        }
     })
 }
